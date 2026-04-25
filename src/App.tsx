dc2ce3a02a65a7eb127207b32f00620f09e798c2
vstaff <vstaff22b@gmail.com>
2026-04-25 20:28:13 +1000

    feat(ui): Форма для получения отзывов о фильме на Кинопоиске по его ID
    Форма включает поле ввода id фильма (число), кнопку подтверждения. После подтверждения формы осуществляется запрос на api, получаем отзывы (см. структуру DTO в src/features/kinopoisk-api/model). Кнопка подтверждения недоступна если input пустой, или осуществляется запрос.
    пока что данные просто выводятся в консоли в браузере. Взаимодействие с LLM по API пока не реализованно.
    также в ui никак не отображаются возможные ошибки (например несуществующий id фильма)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,75 @@
-import { type FC } from 'react';
+import {
+  useState,
+  type ChangeEventHandler,
+  type FC,
+  type SubmitEventHandler,
+} from 'react';
+import { Field, FieldDescription, FieldLabel } from './components/ui/field';
+import { Input } from './components/ui/input';
+import { Button } from './components/ui/button';
+import { getReviewsByID } from './features/kinopoisk-api';
+import { Spinner } from './components/ui/spinner';
 
 const App: FC = () => {
-  return <div className="app">app</div>;
+  const idInputName = 'kinopoisk-id';
+
+  const [userInput, setUserInput] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const handleUserInputChange: ChangeEventHandler<HTMLInputElement> = (
+    event,
+  ) => {
+    const { value } = event.currentTarget;
+    setUserInput(value);
+    setIsSubmitDisabled(value ? false : true);
+  };
+
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState<boolean>(true);
+
+  const handleSubmit: SubmitEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    if (!userInput) {
+      return;
+    }
+
+    setIsSubmitDisabled(true);
+    setIsLoading(true);
+
+    getReviewsByID(+userInput).then((result) => {
+      console.log(result);
+      setIsSubmitDisabled(false);
+      setIsLoading(false);
+    });
+  };
+
+  return (
+    <div className="w-full max-w-md mx-auto p-8">
+      <form action="" onSubmit={handleSubmit} className="flex flex-wrap gap-8">
+        <Field>
+          <FieldLabel htmlFor={idInputName}>kinopoiskId</FieldLabel>
+          <Input
+            value={userInput}
+            onChange={handleUserInputChange}
+            id={idInputName}
+            type="number"
+            min={0}
+            name={idInputName}
+            placeholder="Введите ID фильма на Кинопоиск"
+          />
+          <FieldDescription>
+            см. последний параметр в URL фильма на Кинопоиске
+          </FieldDescription>
+        </Field>
+
+        <Field>
+          <Button type="submit" disabled={isSubmitDisabled}>
+            Подтвердить
+            {isLoading && <Spinner />}
+          </Button>
+        </Field>
+      </form>
+    </div>
+  );
 };
 
 export default App;
